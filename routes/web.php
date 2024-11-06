d5c2fd46a772f0f412945c26026ac200c69e44f8
Rahulkoju <blcraz75@gmail.com>
2024-11-06 14:17:56 +0545

    fix: need authentication for esewa paymment

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,12 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::post('/checkout/esewa', [EsewaController::class, 'checkout'])->name('esewa.checkout');
+    Route::get('/payment/esewa/success', [EsewaController::class, 'success'])->name('esewa.success');
+    Route::get('/payment/esewa/failure', [EsewaController::class, 'failure'])->name('esewa.failure');
+    Route::get('/payment/esewa/status', [EsewaController::class, 'checkStatus'])->name('esewa.status');
 });
 
 Route::get('auth/google', [AuthController::class, 'redirectToGoogle'])->name('auth.google');
 Route::get('auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::post('/checkout/esewa', [EsewaController::class, 'checkout'])->name('esewa.checkout');
-Route::get('/payment/esewa/success', [EsewaController::class, 'success'])->name('esewa.success');
-Route::get('/payment/esewa/failure', [EsewaController::class, 'failure'])->name('esewa.failure');
-Route::get('/payment/esewa/status', [EsewaController::class, 'checkStatus'])->name('esewa.status');
